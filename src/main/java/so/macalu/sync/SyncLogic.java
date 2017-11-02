93725b3001f850b64789a742bac9d9bb5ce5820d
Sam Macaluso <sam@macalu.so>
2017-11-02 19:39:08 -0400

    Going in a different direction making the watcher concurrent

---
+++ src/main/java/so/macalu/sync/SyncLogic.java
@@ -0,0 +1,8 @@
+package so.macalu.sync;
+
+/**
+ * Deals with the logic for a directory, what we want synchronized per connection.
+ */
+public class SyncLogic {
+
+}
---
+++ src/main/java/so/macalu/sync/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The sync package for actually connecting to clients
+ */
+package so.macalu.sync;
